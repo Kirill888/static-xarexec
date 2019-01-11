b3780d0c3ca71723cf597e04209b1156feda1420
Kirill Kouzoubov <kirill888@gmail.com>
2019-01-12 09:31:36 +1100

    De-noise build script output
    - remove verbose flags from untar command

--- docker/build.sh
+++ docker/build.sh
@@ -30,7 +30,7 @@ cd /src
 
 # build static/shared versions of libfuse
 #  `apk add fuse-dev` only includes shared version
-tar xvzf /dl/fuse-2.9.8.tar.gz
+tar xzf /dl/fuse-2.9.8.tar.gz
 (cd fuse-2.9.8 && mkdir B && cd B && \
      ../configure --enable-shared=yes --enable-static=yes --prefix=/usr && \
      make && \
@@ -39,7 +39,7 @@ tar xvzf /dl/fuse-2.9.8.tar.gz
 
 # squashfuse_ll as static binary
 #
-tar xvzf /dl/squashfuse-0.1.103.tar.gz
+tar xzf /dl/squashfuse-0.1.103.tar.gz
 (cd squashfuse-0.1.103 && mkdir B && cd B && \
      ../configure --prefix=/ --with-pic=no --disable-dependency-tracking --disable-demo --disable-high-level --enable-static=yes --enable-shared=no &&\
      make LDFLAGS=-all-static && \
@@ -48,7 +48,7 @@ tar xvzf /dl/squashfuse-0.1.103.tar.gz
 
 
 # (mk|un)squashfs tools with all compressors enabled as a static binary
-tar xvzf /dl/squashfs-tools.tar.gz
+tar xzf /dl/squashfs-tools.tar.gz
 (cd plougher-squashfs-tools-6e242dc/squashfs-tools && \
      make ZSTD_SUPPORT=1 XZ_SUPPORT=1 LZO_SUPPORT=1 LZ4_SUPPORT=1 EXTRA_LDFLAGS=-static && \
      strip mksquashfs &&
@@ -58,7 +58,7 @@ tar xvzf /dl/squashfs-tools.tar.gz
 
 
 # xarexec_fuse as a static binary
-tar xvzf /dl/xar-18.07.12.tar.gz
+tar xzf /dl/xar-18.07.12.tar.gz
 (cd xar-18.07.12 && mkdir B && cd B && \
      cmake -DCMAKE_EXE_LINKER_FLAGS="-static" -DCMAKE_INSTALL_PREFIX:PATH=/ .. &&\
      make && make install/strip
